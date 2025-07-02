c7d999f160784d5209cd42ad90d041b6e94bc275
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-07-02 20:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (61.35%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (char i : s) {
+            if (i == '(' || i == '[' || i == '{') {
+                st.push(i);
+            } else {
+                if (st.empty()) {
+                    return false;
+                }
+                if ((st.top() == '(' && i == ')') ||
+                    (st.top() == '[' && i == ']') ||
+                    (st.top() == '{' && i == '}')
+                )
+                {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
