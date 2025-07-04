e82e3ab2af4c5dbcaceeb43b2a5dc2159eec550c
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-07-04 14:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (14.02%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.cpp
@@ -15,8 +15,21 @@ public:
         if(root == nullptr){
             return 0;
         }
-        int lh = maxDepth(root->left);
-        int rh  = maxDepth(root->right);
-        return 1+max(lh,rh);
+        queue<TreeNode*> q;
+        q.push(root);
+        int count = 0;
+        while(!q.empty()){
+            int size = q.size();
+            vector<int> level;
+            for(int i = 0;i<size;i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+                level.push_back(node->val);
+            }
+            count++;
+        }
+        return count;
     }
 };
