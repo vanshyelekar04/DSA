e88c28e735701809077e15dcf19ce6d3cbfa556d
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-07-27 10:15:48 +0530

    Time: 3 ms (29.29%) | Memory: 17.1 MB (47.10%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,46 +1,47 @@
 class Solution {
 public:
-    
     int orangesRotting(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        queue<pair<pair<int,int>, int>> q;
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j] == 2){
-                    vis[i][j]=2;
-                    q.push({{i,j},0});
-                }
-                else{
-                    vis[i][j] = 0;
+        // define vis-arrray[n,m] and queue{{row, col},time}
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        queue<pair<pair<int, int>, int>> q;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    vis[i][j] = 2;       // mark visited and rotten
+                    q.push({{i, j}, 0}); // initial time is 0
+                } else {
+                    vis[i][j] = 0; // mark not visited and zero
                 }
             }
         }
 
         int time = 0;
-        int drow[] = {-1,0,1,0};
-        int dcol[] = {0,1,0,-1};
-        while(!q.empty()){
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        // BFS
+        while (!q.empty()) {
             int r = q.front().first.first;
             int c = q.front().first.second;
             int t = q.front().second;
-            time = max(time,t);
-
+            time = max(time, t);
             q.pop();
-            for(int i=0;i<4;i++){
-                int nrow = r + drow[i];
-                int ncol = c+ dcol[i];
-                if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && vis[nrow][ncol] !=2 && grid[nrow][ncol] == 1){
-                    q.push({{nrow, ncol},t+1});
+            for (int i = 0; i < 4; i++) {
+                int nrow = r + drow[i]; // traversing to neighbour row
+                int ncol = c + dcol[i]; // traversing to neighbour col
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
+                    vis[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
+                    q.push({{nrow, ncol}, t + 1});
                     vis[nrow][ncol] = 2;
                 }
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(vis[i][j]!=2 && grid[i][j]==1){
+        // check for any fresh oranges
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] != 2 && grid[i][j] == 1) {
                     return -1;
                 }
             }
