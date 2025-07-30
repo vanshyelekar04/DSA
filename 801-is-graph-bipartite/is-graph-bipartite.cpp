60262c6e46d67d93f3d50512d242d9cf726e69dd
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-07-30 20:28:25 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (54.42%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -4,7 +4,7 @@ public:
         vis[node]=c;
         for(int it:graph[node]){
             if(vis[it] == -1){
-                if(!dfs(it, 1-c, vis, graph)) return false;
+                if(dfs(it, !c, vis, graph) == false) return false;
             }else if(vis[it]==vis[node]) return false;
         }
         return true;
@@ -13,8 +13,10 @@ public:
         int n = graph.size();
         vector<int> vis(n,-1);
         for(int i=0;i<n;i++){
-            if(vis[i]==-1 && !dfs(i,0,vis, graph)){
-                return false;
+            if(vis[i]==-1){
+                if(dfs(i,0,vis,graph)==false){
+                    return false;
+                }
             }
         }
         return true;
