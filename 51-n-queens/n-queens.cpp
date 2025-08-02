9c8c6dd34b74ac54e9ad4f376e92868a8bb3429a
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-08-02 11:43:14 +0530

    Time: 3 ms (65.26%) | Memory: 10.1 MB (75.14%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,45 +1,24 @@
 class Solution {
 public:
-    bool isSafe(int row, int col, vector<string>& board, int n) {
-        int duprow = row;
-        int dupcol = col;
 
-        while (row >= 0 && col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            row--;
-            col--;
-        }
-        row = duprow;
-        col = dupcol;
-
-        while (col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            col--;
-        }
-
-        row = duprow;
-        col = dupcol;
-        while (row < n && col >= 0) {
-            if (board[row][col] == 'Q')
-                return false;
-            row++;
-            col--;
-        }
-        return true;
-    }
-    void solve(int col, vector<string>& board, vector<vector<string>>& ans,
-               int n) {
+    void solve(int col, vector<string>& board, vector<vector<string>>& ans, int n,
+        vector<int>& leftRow, vector<int>& lowerDiagonal, vector<int>& upperDiagonal) {
         if (col == n) {
             ans.push_back(board);
             return;
         }
         for (int row = 0; row < n; row++) {
-            if (isSafe(row, col, board, n)) {
+            if(leftRow[row] == 0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row] == 0)
+            {
                 board[row][col] = 'Q';
-                solve(col + 1, board, ans, n);
+                leftRow[row] = 1;
+                lowerDiagonal[row+col]=1;
+                upperDiagonal[n-1+col-row] = 1;
+                solve(col + 1, board, ans, n, leftRow, lowerDiagonal, upperDiagonal);
                 board[row][col] = '.';
+                leftRow[row] = 0;
+                lowerDiagonal[row+col]=0;
+                upperDiagonal[n-1+col-row] = 0;
             }
         }
     }
@@ -50,7 +29,8 @@ public:
         for (int i = 0; i < n; i++) {
             board[i] = s;
         }
-        solve(0, board, ans, n);
+        vector<int> leftRow(n,0), lowerDiagonal(2*n-1,0), upperDiagonal(2*n-1,0);
+        solve(0, board, ans, n, leftRow, lowerDiagonal, upperDiagonal);
         return ans;
     }
 };
