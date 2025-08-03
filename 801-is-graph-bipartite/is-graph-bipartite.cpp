59a5178b77da3d8a20d36c7f940f279080816a37
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-08-03 12:35:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (75.16%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
-    bool helper(int node, int c, vector<int>& vis, vector<vector<int>>& graph){
+    bool dfs(int node, int c, vector<int>& vis, vector<vector<int>>& graph){
         vis[node] = c;
-        queue<int> q;
-        q.push(node);
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            for(auto it:graph[node]){
-                if(vis[it] == -1){
-                    vis[it] = !vis[node];
-                    q.push(it);
-                }else if(vis[it] == vis[node]){
-                    return false;
-                }
-            }
+        for(auto it: graph[node]){
+            if(vis[it] == -1){
+                if(!dfs(it, !c, vis, graph)) return false;
+                } else if(vis[it] == vis[node]) return false;
         }
         return true;
     }
@@ -23,7 +14,7 @@ public:
         vector<int> vis(n,-1);
         for(int i=0;i<n;i++){
             if(vis[i] == -1){
-                if(!helper(i, 0, vis, graph)) return false;
+                if(!dfs(i, 0, vis, graph)) return false;
             }
         }
         return true;
