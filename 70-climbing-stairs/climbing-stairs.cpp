75900e3d7a5bcbed0f9ce9bc1c2e06e2306f798b
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-08-05 09:53:33 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (36.85%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
+    int helper(int n, vector<int>& dp){
+        if(n<=1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = helper(n-1,dp)+helper(n-2,dp);
+    }
     int climbStairs(int n) {
-        if(n==1) return 1;
-        if(n==2) return 2;
-        int prev = 1, prev2 = 1;
-        for(int i=2;i<=n;i++){
-            int temp = prev2;
-            prev2 = prev+prev2;
-            prev = temp;
-        }
-        return prev2;
+        //recursive ->
+        // if(n == 0) return 1;
+        // if(n == 1) return 1;
+        // //jump 1 or jump 2
+        // return climbStairs(n-1) + climbStairs(n-2);
+
+        //convert to memoization ->
+        vector<int> dp(n+1, -1);
+        return helper(n, dp);
     }
 };
