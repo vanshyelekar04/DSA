48345dd7846476a1752910ca8e23b7c8f646a375
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-08-05 12:08:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.56%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,7 +6,7 @@ public:
         int neg = 0;
         for(int i = 1; i<nums.size();i++){
             int pick = nums[i];
-            if(i>1) pick += dp[i-2];
+            if(i>1) pick += dp[i-2]; //negative issue solved
             int notpick = dp[i-1];
             dp[i] = max(pick, notpick);
         }
