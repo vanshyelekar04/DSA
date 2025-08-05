ba3dd9c7788e2ea81da9b9375d520f25beba0ab2
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-08-05 09:06:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.55%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
+    int helper(int n, vector<int>& dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = helper(n-1, dp)+helper(n-2, dp);
+    }
     int fib(int n) {
-        if(n <=1){
-            return n;
-        }
-        return fib(n-1)+fib(n-2);
+        vector<int> dp(n+1,-1);
+        return helper(n, dp);
     }
 };
