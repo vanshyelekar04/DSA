bff31d3350ae13ad413fb5042f16846178a70c2d
VANSH PRAVIN YELEKAR <vanshyelekar04@gmail.com>
2025-09-09 20:14:03 +0530

    Time: 0 ms (100%), Space: 7.8 MB (73.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, 0);
-        dp[0] = 1;
-        dp[1] = 1;
+        if(n<=1) return 1;
+        int prev = 1;
+        int prev2 = 1;
+        int cur = 0;
         for(int i=2;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2];
+            cur = prev2+prev;
+            prev2 = prev;
+            prev = cur;
         }
-        return dp[n];
+        return prev;
     }
 };
